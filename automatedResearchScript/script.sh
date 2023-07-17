62cd097b4d3e0592e59db727a96b8031f4071890
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2023-07-17 18:02:06 +0300

    Merge the two for loops

--- automatedResearchScript/script.sh
+++ automatedResearchScript/script.sh
@@ -1,31 +1,28 @@
+#!/bin/bash -e
 cd "$1"
 
 for folder in ./*
 do
     cd "$folder"
+    
     for cFile in ./*.c
     do
-    /home/pasha/x-tools/riscv64-unknown-elf/bin/riscv64-unknown-elf-gcc -march=rv64gc_zba_zbb_zbc_zbs "$cFile" "$2" -S 
+       /home/pasha/x-tools/riscv64-unknown-elf/bin/riscv64-unknown-elf-gcc \
+        -march=rv64gc_zba_zbb_zbc_zbs "$cFile" "$2" -S 
     done
-    cd ".."
-done
 
-for folder in ./*
-do
-    cd "$folder"
     for assemblyFile in ./*.s
     do
     
-    assemblyText=$( cat "$assemblyFile" ) 
-    instructions=("cpopw" "add.uw" "andn" "clmul" "clmulh" "clmulr" "clz" "clzw" "cpop" "ctz" "ctzw" "max" "rev8" "maxu" "min" "minu" "orc.b" "orn" "rol" "rolw" "ror" "rori" "roriw" "rorw" "bclr" "bclri" "bext" "bexti" "binv" "binvi" "bset" "bseti" "sext.b" "sext.h" "sh1add" "sh1add.uw" "sh2add" "sh2add.uw" "sh3add" "sh3add.uw" "slli.uw" "xnor" "zext.h")
+        assemblyText=$( cat "$assemblyFile" ) 
+        instructions=("cpopw" "add.uw" "andn" "clmul" "clmulh" "clmulr" "clz" "clzw" "cpop" "ctz" "ctzw" "max" "rev8" "maxu" "min" "minu" "orc.b" "orn" "rol" "rolw" "ror" "rori" "roriw" "rorw" "bclr" "bclri" "bext" "bexti" "binv" "binvi" "bset" "bseti" "sext.b" "sext.h" "sh1add" "sh1add.uw" "sh2add" "sh2add.uw" "sh3add" "sh3add.uw" "slli.uw" "xnor" "zext.h")
 
-    for key in "${instructions[@]}"
-    do
-    if echo "$assemblyText" | grep -q -w "$key"; then
-        echo "$key was found in $assemblyFile";
-    fi
-    done
-
+        for instruction in "${instructions[@]}"
+        do
+            if echo "$assemblyText" | grep -q -w "$instruction"; then
+                echo "$key was found in $assemblyFile";
+            fi
+        done
     done
     cd ".."
 done
